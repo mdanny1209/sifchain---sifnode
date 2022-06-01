Sifchain/sifnode

feat: 🎸 new cli to enable swap mechanism

x/clp/handler.go
@@ -53,6 +53,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgUpdateStakingRewardParams:
 			res, err := msgServer.UpdateStakingRewardParams(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgEnableSwap:
+			res, err := msgServer.EnableSwap(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, errors.Wrap(errors.ErrUnknownRequest, errMsg)

mdanny1209/sifchain---sifnode

x/clp/handler.go
@@ -40,7 +40,6 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgUpdatePmtpParams:
 			res, err := msgServer.UpdatePmtpParams(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
-
 		case *types.MsgUnlockLiquidityRequest:
 			res, err := msgServer.UnlockLiquidity(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
@@ -52,6 +51,12 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgUpdateStakingRewardParams:
 			res, err := msgServer.UpdateStakingRewardParams(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgAddSwapAssetPermission:
+			res, err := msgServer.AddSwapAssetPermission(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgRemoveSwapAssetPermission:
+			res, err := msgServer.RemoveSwapAssetPermission(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)